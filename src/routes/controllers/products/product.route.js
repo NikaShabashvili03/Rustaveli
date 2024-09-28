67abdee0b8a0b6b320cb4317ea75a33509c8b6b8
lasha <94359313+Ryugami52@users.noreply.github.com>
2024-09-28 15:27:17 +0400

    Delete src directory

--- src/routes/controllers/products/product.route.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const router = express.Router();
-import * as ProductController from './product.controller.js'
-
-router.post("/create",  ProductController.create)
-router.get("/get", ProductController.get)
-
-
-export default router;
